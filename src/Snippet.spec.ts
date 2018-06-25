13275e45ac65b2f884ed205272a22be0f5004e14
Zeeko <vaezt@outlook.com>
2018-06-25 13:52:40 +0800

    add more test for template parser

--- src/Snippet.spec.ts
+++ src/Snippet.spec.ts
@@ -1,13 +1,12 @@
 import { Snippet } from './Snippet';
 import { expect } from 'chai';
-import { couldStartTrivia } from 'typescript';
 
 describe('Snippet', function () {
     describe('when parse "var $1 = $0"', function () {
         const snippet = new Snippet(['var $1 = $0']);
-        const { tokens, cursorSeq } = snippet.parseBody();
-        it('should have 7 tokens', () => {
-            expect(tokens.length).to.equal(7);
+        const { lexemes, cursorSeq } = snippet.parseBody();
+        it('should have 6 tokens', () => {
+            expect(lexemes.length).to.equal(6);
         });
         it('should have 2 cursors', () => {
             expect(cursorSeq.length).to.equal(2);
@@ -17,10 +16,10 @@ describe('Snippet', function () {
     });
     describe('when parse "const $$ = require(\'$1\')$0"', () => {
         const snippet = new Snippet(['const $$0 = require(\'$1\')$0']);
-        const { tokens, cursorSeq } = snippet.parseBody();
-        it('should have 10 tokens', () => {
-            expect(tokens.length).to.eq(10);
-            const contents = tokens.map(t => t.content);
+        const { lexemes, cursorSeq } = snippet.parseBody();
+        it('should have 9 tokens', () => {
+            expect(lexemes.length).to.eq(9);
+            const contents = lexemes.map(t => t.content);
             expect(contents[0]).to.eq('const ');
             expect(contents[1]).to.eq('$');
             expect(contents[2]).to.eq('$');
@@ -30,12 +29,37 @@ describe('Snippet', function () {
             expect(contents[6]).to.eq('\')');
             expect(contents[7]).to.eq('$');
             expect(contents[8]).to.eq('0');
-            expect(contents[9]).to.eq('\n');
         });
         it('should have 2 cursors', () => {
             expect(cursorSeq.length).to.eq(2);
             expect(cursorSeq[0]).eq(1);
             expect(cursorSeq[1]).eq(0);
+        });
+    });
+
+    describe('parse "$1 test\\n[$2]($3)$0"', () => {
+        const snippet = new Snippet(['$1 test', '[$2]($3)$0']);
+        const { lexemes, cursorSeq } = snippet.parseBody();
+        const contents = lexemes.map(t => t.content);
+        it('should have 13 tokens', () => {
+            expect(lexemes.length).to.eq(13);
+            expect(contents[0]).to.eq('$');
+            expect(contents[1]).to.eq('1');
+            expect(contents[2]).to.eq(' test');
+            expect(contents[3]).to.eq('\n');
+            expect(contents[4]).to.eq('[');
+            expect(contents[5]).to.eq('$');
+            expect(contents[6]).to.eq('2');
+            expect(contents[7]).to.eq('](');
+            expect(contents[8]).to.eq('$');
+            expect(contents[9]).to.eq('3');
+            expect(contents[10]).to.eq(')');
+            expect(contents[11]).to.eq('$');
+            expect(contents[12]).to.eq('0');
+        });
+
+        it('should have 4 cursors', () => {
+            expect(cursorSeq.length).to.eq(4);
         });
     });
 });
--- src/Snippet.ts
+++ src/Snippet.ts
@@ -10,6 +10,8 @@ interface IPosition {
     line: number;
     ch: number;
 }
+type SnippetParserState = 0 | 1 | 2 | 3 | 4 | 5;
+
 
 export class Snippet {
     static varToken = /^\$$/;
@@ -20,22 +22,25 @@ export class Snippet {
     }
 
     public parseBody() {
+        const isAcceptable = (state: number) => {
+            return [0, 3, 4, 5].indexOf(state) >= 0;
+        };
         const cursorSeq: number[] = [];
-        const tokens: ISnippetLexeme[] = [];
+        const lexemes: ISnippetLexeme[] = [];
         for (let i = 0; i < this.body.length; i++) {
-            let state: 0 | 1 | 2 | 3 | 4 | 5 = 1;
+            let state: SnippetParserState = 1;
             let begin = 0;
-            for (let end = 0; end <= this.body[i].length; end++) {
+            for (let end = 1; end <= this.body[i].length; end++) {
                 const str = this.body[i].substring(begin, end);
-                if (state === 0) { // token accpeted, then start a new trun;
+                if (state === 0) { // token accepted, then start a new trun;
                     state = 1;
                 }
                 switch (state) {
-                    case 1:
+                    case 1: // begin
                         if (Snippet.varToken.test(str)) {
                             state = 2; // $
-                            begin = end; // accpect the leading $ and expect $ or number;
-                            tokens.push({
+                            begin = end; // accept the leading $ and expect $ or number;
+                            lexemes.push({
                                 type: '$', content: '$',
                                 position: {
                                     from: { line: i, ch: begin },
@@ -46,11 +51,11 @@ export class Snippet {
                             state = 5; // other string
                         }
                         break;
-                    case 2:
+                    case 2: // $
                         if (Snippet.varToken.test(str)) {
                             state = 3; // $$
                             begin = end;
-                            tokens.push({
+                            lexemes.push({
                                 type: 'string', content: '$',
                                 position: {
                                     from: { line: i, ch: begin },
@@ -62,7 +67,7 @@ export class Snippet {
                             state = 4; // $n
                             begin = end;
                             cursorSeq.push(parseInt(str, 10));
-                            tokens.push({
+                            lexemes.push({
                                 type: 'var', content: str,
                                 position: {
                                     from: { line: i, ch: begin },
@@ -74,12 +79,17 @@ export class Snippet {
                             throw Error('Expected "$" or number');
                         }
                         break;
-                    case 5:
-                        if (Snippet.otherToken.test(str) === false) {
-                            end--;
+                    case 5: // plain text
+                        if (Snippet.otherToken.test(str) === false || end === this.body[i].length) {
+                            if (end !== this.body[i].length) {
+                                end--;
+                            }
+                            if (begin === end) {
+                                break; // ignore empty string
+                            }
                             const tokenContent = this.body[i].substring(begin, end);
                             begin = end;
-                            tokens.push({
+                            lexemes.push({
                                 type: 'string', content: tokenContent,
                                 position: {
                                     from: { line: i, ch: begin },
@@ -91,18 +101,20 @@ export class Snippet {
                         break;
                 }
             }
-            if (state !== 0) { // parse complete
+            if (isAcceptable(state) === false) { // parse complete
                 throw Error('Invalid template');
             }
-            tokens.push({
-                type: 'string', content: '\n',
-                position: {
-                    from: { line: i, ch: this.body.length },
-                    to: { line: i, ch: this.body.length }
-                }
-            });
+            if (i !== this.body.length - 1) {
+                lexemes.push({
+                    type: 'string', content: '\n',
+                    position: {
+                        from: { line: i, ch: this.body.length },
+                        to: { line: i, ch: this.body.length }
+                    }
+                });
+            }
         }
-        return { tokens, cursorSeq };
+        return { lexemes, cursorSeq };
     }
 
     constructor(public body: string[], public prefix?: string) {
