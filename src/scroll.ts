934e07bc53a790adb130e04c4d42cedee82be1a5
Jerry <jerry@mail.cnblogs.com>
2018-06-27 15:42:49 +0800

    disable sync scroll when hide preview

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,5 +1,5 @@
 import { fromEvent } from 'rxjs';
-import { takeUntil, map, distinctUntilChanged } from 'rxjs/operators';
+import { takeUntil, map, distinctUntilChanged, filter } from 'rxjs/operators';
 import { LineScrolled } from './yame.service';
 import Yame from './yame';
 
@@ -79,9 +79,11 @@ export const getDataLine = (el: Element) => {
 export const InitScroll = (yame: Yame) => {
     const $editorScroll = fromEvent(yame.editor, 'scroll').pipe(
         takeUntil(yame.uiStore.unSub),
+        filter(_ => this.uiStore.model.enablePreview === true),
     );
     const $previewScroll = fromEvent(yame.ui.previewHost, 'scroll').pipe(
         takeUntil(yame.uiStore.unSub),
+        filter(_ => this.uiStore.model.enablePreview === true),
     );
     $editorScroll.subscribe(() => {
         // 编辑器滚动时，获取显示在第一行的代码的行号
